c579d2db8463b77e12002989f99f50231c50af1c
Fatihcan Uslu <19360859048@ogrenci.btu.edu.tr>
2023-06-09 23:23:56 +0300

    signup formu bağlandı

--- web modules/src/index.js
+++ web modules/src/index.js
@@ -2,30 +2,39 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const hbs=require('hbs');
-const templatePath=path.join(__dirname,'../templates');
+const templatePath=path.join(__dirname,'../sof_eng_project');
 const collection=require('./mongo'); //mongodb.js dosyasından dönen collection buradan çağırılıyor
 
 app.use(express.json());
 app.set('view engine','hbs');//görüntü motorunun hbs olduğu tanımlanıyor
 app.set("views",templatePath);//templetePath değişkeni views klasörünün yolu
-app.use(express.urlencoded({extended:false}))
-
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(templatePath));
 app.get('/',(req,res)=>{
-    res.render('login'); //login.hbs
+    res.sendFile(templatePath+'/index.html');
 });
-app.get('/signup',(req,res)=>{
-    res.render('signup'); //signup.hbs
-});
+
+
 
 //post kısmı
-app.post('/signup',async(req,res)=>{
+app.post('/signup', async (req, res) => {
     const data={
         name:req.body.name,
+        phone_number:req.body.phone_number,
+        email:req.body.email,
         password:req.body.password
     }
+    console.log('Kullanıcı adı:', data.name);
+    console.log('E-posta:', data.email);
+    console.log('Şifre:', data.password);
     await collection.insertMany([data])
-    res.render("home") 
+    // Verileri kullanarak istediğiniz işlemleri gerçekleştirin
+    
+    //res.sendFile(templatePath+'/index.html');
+    res.send("You have been succesfully signed up");
 });
+
+
 
 app.post('/login',async(req,res)=>{
 
